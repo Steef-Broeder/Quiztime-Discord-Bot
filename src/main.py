e61db2d48842412c48deab0d319276381045a554
Steef Broeder <steef@broeder.co.nl>
2022-10-20 11:43:29 +0200

    personalized reactoins

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import discord
 import json
 
@@ -6,19 +7,29 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+reactions = {
+    "Silly":"Ga lekker onder n truck liggen man",
+    "xshadowshots":"Jaja hou je bek maar",
+    "CoasterCrazy42":"Ik hoop dat je achtbaan breekt"
+}
+
 with open('config.json', 'r') as cfg:
   data = json.load(cfg) 
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.channel.name == 'bot-testing':
+        if message.author.name != 'Broeder':
+            try:
+                await message.channel.send(reactions[message.author.name])
+            except:
+                await message.channel.send(f"Hou je bek {message.author.name}")
 
 client.run(token=data["token"])
