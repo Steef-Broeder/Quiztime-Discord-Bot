c63221e538b03ebdea9b1b45194c770e7f90a2e4
Steef <steef@broeder.co.nl>
2022-10-27 01:00:48 +0200

    moved the current discord bot code to a file

---
+++ src/discordtest.py
@@ -0,0 +1,24 @@
+import discord
+import json
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+with open('config.json', 'r') as cfg:
+  data = json.load(cfg) 
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(token=data["discord_token"])
